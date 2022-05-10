3a3b6b83c0036b98bc9e2d2d01095a1e706a54b7
Chris Romero <chrisleeromero1@gmail.com>
2022-05-09 19:26:16 -0600

    feat: create many to many relationship for parties and users with PartyUsers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_10_012231) do
+ActiveRecord::Schema.define(version: 2022_05_10_012448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2022_05_10_012231) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "party_users", force: :cascade do |t|
+    t.bigint "users_id"
+    t.bigint "parties_id"
+    t.index ["parties_id"], name: "index_party_users_on_parties_id"
+    t.index ["users_id"], name: "index_party_users_on_users_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -32,4 +39,6 @@ ActiveRecord::Schema.define(version: 2022_05_10_012231) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "party_users", "parties", column: "parties_id"
+  add_foreign_key "party_users", "users", column: "users_id"
 end
