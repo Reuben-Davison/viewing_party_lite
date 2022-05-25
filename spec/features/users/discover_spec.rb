0622a7eac6045c3edb69e6a7099d4580843568f1
reuben <creubend@gmail.com>
2022-05-25 07:48:59 -0600

    test: refactor test to remove id from routes for user show spec

--- spec/features/users/discover_spec.rb
+++ spec/features/users/discover_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe 'discover page' do
     it 'has a top rated movies link' do
         user = User.create!(name: "will", email: "will@g.com", password: "test", password_confirmation: "test")
-        visit "/users/#{user.id}/discover"
+        visit "/discover"
         click_button 'Top Movies'
         expect(current_path).to eq('/movies/top_movies')
         expect(page).to have_content("Top Movies")
         expect(page).to have_content("Shawshank Redemption - With an average rating of: 8.7")
         expect(page).to_not have_content("Fight Club")
         click_button 'Return to Discover Page'
-        expect(current_path).to eq("/users/#{user.id}/discover")
+        expect(current_path).to eq("/discover")
     end
 
 end
--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -2,18 +2,28 @@ require 'rails_helper'
 
 RSpec.describe "user show page" do
     it  'has a users dashboard' do
-        frank = User.create!(name: "Frank", email: "frank@yahoo.com", password: "test", password_confirmation: "test")
-        visit "/users/#{frank.id}"
+        visit '/register'
+        fill_in "name",	with: "Rupert" 
+        fill_in "email",	with: "rupert@gmail.com" 
+        fill_in "password",	with: "password1" 
+        fill_in "password_confirmation",	with: "password1" 
+        click_button 'Register'        
+        visit "/dashboard"
 
-        expect(page).to have_content("Frank's Dashboard")
+        expect(page).to have_content("Rupert's Dashboard")
         expect(page).to have_button("Discover Movies")
         expect(page).to have_content("Viewing Parties")
     end
     it 'has a button that takes the user to a discover page' do
-      frank = User.create!(name: "Frank", email: "frank@yahoo.com", password: "test", password_confirmation: "test")
-      visit "/users/#{frank.id}"
+        visit '/register'
+        fill_in "name",	with: "Rupert" 
+        fill_in "email",	with: "rupert@gmail.com" 
+        fill_in "password",	with: "password1" 
+        fill_in "password_confirmation",	with: "password1" 
+        click_button 'Register'     
+        visit "/dashboard"
 
       click_on 'Discover Movies'
-      expect(current_path).to eq("/users/#{frank.id}/discover")
+      expect(current_path).to eq("/discover")
     end
 end
